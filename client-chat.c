1ea9e8bdd364f0bc1ba6ac4768559eeae08441f4
SosoMilk <sofiailin10@gmail.com>
2024-05-22 16:59:46 -0300

    chat cliente servidor

--- client-chat.c
+++ client-chat.c
@@ -7,70 +7,64 @@
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
     int sock = 0;
     struct sockaddr_in serv_addr;
     char buffer[BUFFER_SIZE] = {0};
     char message[BUFFER_SIZE] = {0};
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\nError al crear el socket \n");
         return -1;
     }
+
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
-        printf("\nInvalid address/ Address not supported \n");
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
+        printf("\nDirección no válida o no soportada \n");
         return -1;
     }
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
-    
-    // Mensaje de conexión establecida
-    int valread = read(sock, buffer, BUFFER_SIZE);
-    if (valread == -1) {
-        perror("read");
-        return -1;
-    }
-    printf("Server replied: %s\n", buffer);
 
-    while(1) {
-        printf("Enter message: ");
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nError de conexión \n");
+        return -1;
+    }
+
+    while (1)
+    {
+        printf("Ingrese mensaje: ");
         fgets(message, BUFFER_SIZE, stdin);
-        
-        // Eliminar el salto de línea del final de la cadena
+
         size_t len = strlen(message);
-        if (len > 0 && message[len-1] == '\n') {
+        if (len > 0 && message[len - 1] == '\n')
+        {
             message[--len] = '\0';
         }
-        
+
         send(sock, message, strlen(message), 0);
-        
-        // Limpiar el buffer antes de recibir la respuesta del servidor
+
         memset(buffer, 0, BUFFER_SIZE);
-        
-        valread = read(sock, buffer, BUFFER_SIZE);
-        if (valread == -1) {
-            perror("read");
+        int valread = read(sock, buffer, BUFFER_SIZE);
+        if (valread == -1)
+        {
+            perror("Error al leer del socket");
             return -1;
         }
-        printf("Server replied: %s\n", buffer);
-        
-        // Verificar si el cliente quiere desconectar
-        if (strcmp(message, "disconnect") == 0) {
-            printf("Disconnecting from server...\n");
-            break; // Salir del bucle para cerrar la conexión
+
+        printf("Respuesta del servidor: %s\n", buffer);
+
+        if (strcmp(message, "disconnect") == 0)
+        {
+            printf("Desconectando del servidor...\n");
+            break;
         }
     }
 
-    // Cerrar el socket antes de salir del programa
     close(sock);
-
     return 0;
 }
-
-
--- server-chat.c
+++ server-chat.c
@@ -1,7 +1,125 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <errno.h>
 
-int main(int argc, char* argv[])
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main(int argc, char *argv[])
 {
-    exit(EXIT_SUCCESS);
+    int sockfd, max_sd, sd, new_socket, activity, i, valread;
+    int client_socket[30] = {0};
+    struct sockaddr_in servaddr, cliaddr;
+    fd_set readfds;
+    char buffer[BUFFER_SIZE];
+
+    // Crear socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        perror("Error al crear socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configurar dirección y puerto del servidor
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
+
+    // Vincular el socket con la dirección y puerto
+    if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        perror("Error al vincular");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Escuchar por conexiones entrantes
+    if (listen(sockfd, 3) < 0)
+    {
+        perror("Error al escuchar");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Servidor escuchando en el puerto %d\n", PORT);
+
+    // Aceptar conexiones entrantes
+    while (1)
+    {
+        FD_ZERO(&readfds);
+        FD_SET(sockfd, &readfds);
+        max_sd = sockfd;
+
+        for (i = 0; i < 30; i++)
+        {
+            sd = client_socket[i];
+            if (sd > 0)
+                FD_SET(sd, &readfds);
+            if (sd > max_sd)
+                max_sd = sd;
+        }
+
+        activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+        if ((activity < 0) && (errno != EINTR))
+        {
+            perror("Error en select");
+        }
+
+        if (FD_ISSET(sockfd, &readfds))
+        {
+            socklen_t cli_len = sizeof(cliaddr);
+            new_socket = accept(sockfd, (struct sockaddr *)&cliaddr, &cli_len);
+            if (new_socket < 0)
+            {
+                perror("Error al aceptar conexión");
+                exit(EXIT_FAILURE);
+            }
+
+            printf("Nueva conexión, socket fd es %d, IP es: %s, puerto: %d\n",
+                   new_socket, inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
+
+            for (i = 0; i < 30; i++)
+            {
+                socklen_t cli_len = sizeof(cliaddr);
+                if (client_socket[i] == 0)
+                {
+                    client_socket[i] = new_socket;
+                    printf("Añadiendo a la lista de sockets como %d\n", i);
+                    break;
+                }
+            }
+        }
+
+        for (i = 0; i < 30; i++)
+        {
+            sd = client_socket[i];
+            if (FD_ISSET(sd, &readfds))
+            {
+                if ((valread = read(sd, buffer, BUFFER_SIZE)) == 0)
+                {
+                    getpeername(sd, (struct sockaddr *)&cliaddr, &cli_len);
+                    printf("Host desconectado, IP %s, puerto %d\n",
+                           inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
+                    close(sd);
+                    client_socket[i] = 0;
+                }
+                else
+                {
+                    buffer[valread] = '\0';
+                    printf("Mensaje recibido: %s\n", buffer);
+                    send(sd, buffer, strlen(buffer), 0);
+                }
+            }
+        }
+    }
+
+    close(sockfd);
+    return 0;
 }
